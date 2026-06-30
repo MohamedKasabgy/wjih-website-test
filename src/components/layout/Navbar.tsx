bde89be4bc6aae2c1d62b92cc676e1c2d89e2f92
Ibrahim Eissa Abu Alghayth <ibrahimapps102938@gmail.com>
2026-07-01 01:47:05 +0300

    UI enhancements: Increase loading screen duration to 1.4s and elevate Navbar z-index to stay above loading overlay

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -48,7 +48,7 @@ export function Navbar({ isLoading }: { isLoading?: boolean }) {
 
   return (
     <motion.header
-      className="fixed left-0 right-0 z-40 w-full"
+      className="fixed left-0 right-0 z-[70] w-full"
       style={headerStyle}
     >
       <Container>
